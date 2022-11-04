3125beb091dec3fc0638e981399bb55af1908e0e
김민주 <77184157+kim-min-ju449@users.noreply.github.com>
2022-11-04 13:46:15 +0900

    firebase commit

--- src/firebase.js
+++ src/firebase.js
@@ -5,11 +5,11 @@ import "firebase/compat/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: ,
-    projectId:,
-    storageBucket: ,
-    messagingSenderId:,
-    appId: ,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET ,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
     measurementId: process.env.REACT_MEASUREMENT_ID
   };
   firebase.initializeApp(firebaseConfig);
