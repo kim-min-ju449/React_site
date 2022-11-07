73990f495795b901c3797fede40bc74754eed9f0
김민주 <77184157+kim-min-ju449@users.noreply.github.com>
2022-11-08 08:48:24 +0900

    이미지 등록 성공

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,37 +1,37 @@
 import React from "react";
 import {useEffect, useState} from "react";
 import {dbService} from "../firebase";
+import Nweet from "../components/Nweet";
 
-const Home = () => {
+const Home = ({userObj}) => {
+    
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment] = useState("");
 
-    const getNweets = async () =>{
-        const dbNweets = await dbService.collection("nweets").get();
-        //console.log(dbNweets);
-        console.log("getNweets() 실행");
-        dbNweets.forEach((document) => {
-            //console.log(document.data());
-            const nweetObject = { ...document.data(), id:document.id};
-            setNweets((prev) => [nweetObject, ...prev])
+    useEffect(() => {
+        dbService.collection("nweets").onSnapshot((snapshot) => {
         
-    });
-    };
-
-    useEffect(() =>{
-        getNweets();
-        console.log("유즈이펙트 호출");
-    },[]);
+          const newArray = snapshot.docs.map((document) => ({
+            id: document.id,
+            ...document.data(),
+          }));
+          setNweets(newArray);
+        });
+      }, []);
     //console.log(nweets);
 
     const onSubmit = async (event) =>{
-        console.log("onSubmit() 호출");
+        //console.log(userObj.uid)
         event.preventDefault();
         await dbService.collection("nweets").add({
-            text:nweet,
+            text: nweet,
+            createdAt: Date.now(),
+            //creatorId: userObj.uid,
             createdAt: Date.now(),
         });
         setNweet("");
+        
     }
 
     const onChange = (event) =>{
@@ -43,6 +43,25 @@ const Home = () => {
         setNweet(value);
     };
 
+    const onFileChange = (event) =>{
+       const{
+        target:{files},
+
+       } = event;
+       const theFile = files[0];
+       const reader = new FileReader();
+       reader.onloadend = (finishedEvent) =>{
+        //console.log(finishedEvent);
+            const{
+                currentTarget: {result},
+            } = finishedEvent;
+            setAttachment(result);
+       };
+       reader.readAsDataURL(theFile);
+    }
+
+    const onClearAttachment = () => setAttachment("");
+
     return(
         <>
         <form onSubmit={onSubmit}>
@@ -53,22 +72,34 @@ const Home = () => {
             placeholder="What is on your mind?"
             maxLength={120}
             />
+            <input type="file" accept="image/*" onChange={onFileChange}/>
             <input type="submit" value="Nweet"/>
+            {attachment && (
+                <div>
+                <img src={attachment} width="50px" height="50px"/>
+                <button onClick={onClearAttachment}>Clear</button>
+            </div>
+            )}
         </form>
         <div>
             {nweets.map((nweet) => (
-                <div key={nweet.id}>
-                    <h4>{nweet.text}</h4> 
-                </div>
+                // <div key={nweet.id}>
+                //    <h4>{nweet.text}</h4> 
+                // </div>
+                <Nweet
+                    key={nweet.id}
+                    nweetObj={nweet}/>
+                    //isOwner={nweet.creatorId === userObj.uid}
+                ///>
+               
             ))}
+            
+            
         </div>
     </>
+    
     );
-
-
-
-};
-
+    }
 
 
 export default Home;
